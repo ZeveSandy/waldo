2b02b5d02f431ae2d6a7f0cc14f797a595b7ee3e
Zeve Sanderson <zevesanderson@gmail.com>
2013-08-26 00:16:38 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waldo::Application.config.secret_token = '2819d43a133c43bf27559827438c26da0fab199e2ee131e34bb4d3de39e63de27763fd737489721fec4d9403605c9449a61b5ac93b465808a010d3b0e32d40b5'
